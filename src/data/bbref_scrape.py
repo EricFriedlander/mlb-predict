bb713dd96381353c49dadf351659d73a10f7a578
Eric Friedlander <EricF2218@gmail.com>
2020-02-20 20:26:27 -0600

    Added method for scraping scorebox info.

--- src/data/bbref_scrape.py
+++ src/data/bbref_scrape.py
@@ -18,7 +18,20 @@ class BoxScore(object):
 
     """
 
+    def set_score_box_info(self, away, home, date, time, attendance, venue, duration, time_place):
+        """Sets all values associated with the score_box"""
+        self.away_team = away
+        self.home_team = home
+        self.date = date
+        self.time = time
+        self.attendance = attendance
+        self.venue = venue
+        self.duration = duration
+        self.time_place = time_place
+
+       
     def set_linescore(self, df):
+        """ Sets the linescore"""
         self.linescore = df
 
 class BoxScoreScraper(object):
@@ -40,7 +53,7 @@ class BoxScoreScraper(object):
         # Comments cause scraping to fail so we must remove them then and scrape
         comments = re.compile("<!--|-->")
         self.soup = bs4.BeautifulSoup(comments.sub('', self.response.text), 'lxml')
-        self.divs = self.soup.find('div', id = "content")
+        self.content = self.soup.find('div', id = "content")
 
         # Create boxScore object to be populated
         self.box_score = BoxScore()
@@ -48,9 +61,36 @@ class BoxScoreScraper(object):
         # Scrape linescore table
         self.scrape_linescore()
 
+    def scrape_scorebox(self):
+        """Scrapes data from scorebox including home/away teams, date, start time, attendance, venue, duration, 
+        then puts it in the classes BoxScore object."""
+        scorebox = self.content.find('div', {'class' : 'scorebox'})
+
+        # Get home/away teams
+        teams = scorebox.find_all('a', {'itemprop' : "name"})
+        away_team = teams[0].text
+        home_team = teams[1].text
+
+        # Get meta information
+        scorebox_meta = scorebox.find('div', {'class' : 'scorebox_meta'}).find_all('div')
+        date = scorebox_meta[0].text
+        time = scorebox_meta[1].text.split(':', 1)[1].strip()
+        attendance = scorebox_meta[2].text.split(':', 1)[1].strip()
+        attendance = int(attendance.replace(',', ''))
+        venue = scorebox_meta[3].text.split(':', 1)[1].strip()
+        duration = scorebox_meta[4].text.split(':', 1)[1].strip()
+        time_place = scorebox_meta[5].text.strip()
+
+        # Put information in BoxScore object
+        self.box_score.set_score_box_info(away_team, home_team, date, time, attendance, venue, duration, time_place)
+
+
+
+
     def scrape_linescore(self):
-        """Scrapes basic overview of game including runs in each inning, hits, errors, final score, teams, and pitchers."""
-        table = self.divs.find('table', {'class' : "linescore"})
+        """Scrapes basic overview of game including runs in each inning, hits, errors, final score, teams, and pitchers, 
+        then puts it in the classes BoxScore object."""
+        table = self.content.find('table', {'class' : "linescore"})
         df = pd.read_html(table.prettify(), flavor='lxml')[0]
         df = df.drop(df.columns[0], axis=1)
         df = df.drop(2)
